a2923b4e2e43477d0ad88c28e751418b8f59e38c
Rajab Natshah <rajabn@gmail.com>
2019-11-18 15:11:53 +0200

    Added a Target directory for which New profiles will be generated in

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Varbase Subprofile Generator
 
 ```
-cd /var/www/html/products
-git clone git@github.com:Vardot/varbase-subprofile-generator.git
-cd /var/www/html/products/varbase-subprofile-generator
-bash generate-varbase-subprofile.sh "cv" -vvv
+$ mkdir /var/www/html/products
+$ cd /var/www/html/products
+$ git clone git@github.com:Vardot/varbase-subprofile-generator.git
+$ cd /var/www/html/products/varbase-subprofile-generator
+$ bash generate-varbase-subprofile.sh "cv" "/var/www/html/products" -vvv
 ll
 
 ```
@@ -12,9 +13,19 @@ ll
 Then you will see the following
 
 ```
-create-new-varbase-subprofile.sh
+$ cd /var/www/html/products 
+$ ll
 cv/
 cv-project/
-.git/
-README.md
 ```
+
+After that you will need to push the generated profile and project template to
+https://github.com for example and submit them to https://packagist.org
+
+or commit to a project in https://drupal.org
+
+As we do have that at
+* https://github.com/Vardot/cv
+* https://github.com/Vardot/cv-project
+And
+* https://www.drupal.org/project/cv
--- generate-varbase-subprofile.sh
+++ generate-varbase-subprofile.sh
@@ -1,6 +1,7 @@
 #!/bin/usr/env bash
 
 current_path=$(pwd);
+target_path="${current_path}";
 user_name="$USER";
 
 subprofile_name="profilly"
@@ -12,90 +13,108 @@ if [ ! -z "$1" ]; then
     subprofile_name="$arg1";
   else
     echo "---------------------------------------------------------------------------";
-    echo "   Theme name is not a valid profile name!                                   ";
+    echo "   Profile name is not a valid profile name!                               ";
     echo "---------------------------------------------------------------------------";
     exit 1;
   fi
 else
   echo "---------------------------------------------------------------------------";
-  echo "   Please add the name of profile theme!                                      ";
+  echo "   Please add the name of the profile!                                   ";
   echo "---------------------------------------------------------------------------";
   exit 1;
 fi
 
-## Processor: Public Github finished generating.
-echo "=========================================================================";
-echo "  Processor: Public Github finished generating                       ";
-echo "=========================================================================";
+# Grape the target path argument.
+if [ ! -z "$2" ]; then
+  arg2="$2";
+  if [[ -d "${arg2}" ]]; then
+    target_path="$arg2";
+  else
+    echo "---------------------------------------------------------------------------";
+    echo "   Target directory is not valid or you do not have permission!            ";
+    echo "   Please, create the target directory, and provide the full target path   ";
+    echo "   For Example:";
+    echo "   $ bash generate-varbase-subprofile.sh "cv" "/var/www/html/products" -vvv";
+    echo "---------------------------------------------------------------------------";
+    exit 1;
+  fi
+else
+  echo "---------------------------------------------------------------------------";
+  echo "   As you did not provide any Target path                                  ";
+  echo "   New profiles will be generated in:                                      ";
+  echo "    ${current_path}                                                        ";
+  echo "---------------------------------------------------------------------------";
+fi
+
 
 # Create the target [Varbase Subprofile Basic] for the profile.
 # =============================================================================
-if [[ -d "${current_path}/$subprofile_name" ]]; then
- rm -rf ${current_path}/$subprofile_name ;
+if [[ -d "${target_path}/$subprofile_name" ]]; then
+ rm -rf ${target_path}/$subprofile_name ;
 fi
 
-git clone git@github.com:Vardot/varbase_subprofile_basic.git $subprofile_name ;
+git clone git@github.com:Vardot/varbase_subprofile_basic.git ${target_path}/${subprofile_name} ;
 
-cd ${current_path}/${subprofile_name} ;
+cd ${target_path}/${subprofile_name} ;
 rm -rf .git ;
 rename "s/varbase_subprofile_basic/${subprofile_name}/g" *
 grep -rl 'varbase_subprofile_basic' * | xargs sed -i "s/varbase_subprofile_basic/${subprofile_name}/g"
 grep -rl 'Varbase Sub Profile Basic' * | xargs sed -i "s/Varbase Sub Profile Basic/${subprofile_name}/g"
 grep -rl 'Varbase Subprofile' * | xargs sed -i "s/Varbase Subprofile/${subprofile_name}/g"
 
-cd ${current_path}/${current_path}/${subprofile_name}/config/install
+cd ${target_path}/${target_path}/${subprofile_name}/config/install
 rename "s/varbase_subprofile_basic/${subprofile_name}/g" *
 grep -rl 'varbase_subprofile_basic' * | xargs sed -i "s/varbase_subprofile_basic/${subprofile_name}/g"
 grep -rl 'Varbase Sub Profile Basic' * | xargs sed -i "s/Varbase Sub Profile Basic/${subprofile_name}/g"
 grep -rl 'Varbase Subprofile' * | xargs sed -i "s/Varbase Subprofile/${subprofile_name}/g"
 
-cd ${current_path}/${subprofile_name}/configbit
+cd ${target_path}/${subprofile_name}/configbit
 rename "s/varbase_subprofile_basic/${subprofile_name}/g" *
 grep -rl 'varbase_subprofile_basic' * | xargs sed -i "s/varbase_subprofile_basic/${subprofile_name}/g"
 grep -rl 'Varbase Sub Profile Basic' * | xargs sed -i "s/Varbase Sub Profile Basic/${subprofile_name}/g"
 grep -rl 'Varbase Subprofile' * | xargs sed -i "s/Varbase Subprofile/${subprofile_name}/g"
 
-cd ${current_path}/${subprofile_name}/modules
+cd ${target_path}/${subprofile_name}/modules
 rename "s/varbase_subprofile_basic/${subprofile_name}/g" *
 grep -rl 'varbase_subprofile_basic' * | xargs sed -i "s/varbase_subprofile_basic/${subprofile_name}/g"
 grep -rl 'Varbase Sub Profile Basic' * | xargs sed -i "s/Varbase Sub Profile Basic/${subprofile_name}/g"
 grep -rl 'Varbase Subprofile' * | xargs sed -i "s/Varbase Subprofile/${subprofile_name}/g"
 
-cd ${current_path}/${subprofile_name}/modules/${subprofile_name}_demo
+cd ${target_path}/${subprofile_name}/modules/${subprofile_name}_demo
 rename "s/varbase_subprofile_basic/${subprofile_name}/g" *
 grep -rl 'varbase_subprofile_basic' * | xargs sed -i "s/varbase_subprofile_basic/${subprofile_name}/g"
 grep -rl 'Varbase Sub Profile Basic' * | xargs sed -i "s/Varbase Sub Profile Basic/${subprofile_name}/g"
 grep -rl 'Varbase Subprofile' * | xargs sed -i "s/Varbase Subprofile/${subprofile_name}/g"
 
-cd ${current_path}/${subprofile_name}/modules/${subprofile_name}_features
+cd ${target_path}/${subprofile_name}/modules/${subprofile_name}_features
 rename "s/varbase_subprofile_basic/${subprofile_name}/g" *
 grep -rl 'varbase_subprofile_basic' * | xargs sed -i "s/varbase_subprofile_basic/${subprofile_name}/g"
 grep -rl 'Varbase Sub Profile Basic' * | xargs sed -i "s/Varbase Sub Profile Basic/${subprofile_name}/g"
 grep -rl 'Varbase Subprofile' * | xargs sed -i "s/Varbase Subprofile/${subprofile_name}/g"
 
-cd ${current_path}/${subprofile_name}/modules/${subprofile_name}_homepage
+cd ${target_path}/${subprofile_name}/modules/${subprofile_name}_homepage
 rename "s/varbase_subprofile_basic/${subprofile_name}/g" *
 grep -rl 'varbase_subprofile_basic' * | xargs sed -i "s/varbase_subprofile_basic/${subprofile_name}/g"
 grep -rl 'Varbase Sub Profile Basic' * | xargs sed -i "s/Varbase Sub Profile Basic/${subprofile_name}/g"
 grep -rl 'Varbase Subprofile' * | xargs sed -i "s/Varbase Subprofile/${subprofile_name}/g"
 
-cd ${current_path}/${subprofile_name}/modules/${subprofile_name}_homepage/config/install
+cd ${target_path}/${subprofile_name}/modules/${subprofile_name}_homepage/config/install
 rename "s/varbase_subprofile_basic/${subprofile_name}/g" *
 grep -rl 'varbase_subprofile_basic' * | xargs sed -i "s/varbase_subprofile_basic/${subprofile_name}/g"
 grep -rl 'Varbase Sub Profile Basic' * | xargs sed -i "s/Varbase Sub Profile Basic/${subprofile_name}/g"
 grep -rl 'Varbase Subprofile' * | xargs sed -i "s/Varbase Subprofile/${subprofile_name}/g"
 
-cd ${current_path}
+cd ${target_path}
 
 # Create the target [Varbase Sub Profile Basic Project] for the profile.
 # =============================================================================
-if [[ -d "${current_path}/${subprofile_name}-project" ]]; then
-  rm -rf ${current_path}/${subprofile_name}-project
+if [[ -d "${target_path}/${subprofile_name}-project" ]]; then
+  rm -rf ${target_path}/${subprofile_name}-project
 fi
 
-git clone git@github.com:Vardot/varbase-subprofile-basic-project.git ${subprofile_name}-project
+git clone git@github.com:Vardot/varbase-subprofile-basic-project.git ${target_path}/${subprofile_name}-project
 
-cd ${current_path}/${subprofile_name}-project
+cd ${target_path}/${subprofile_name}-project
 rm -rf .git
 
 grep -rl 'varbase_subprofile_basic' * | xargs sed -i "s/varbase_subprofile_basic/${subprofile_name}/g"
@@ -104,3 +123,8 @@ grep -rl 'Varbase Subprofile' * | xargs sed -i "s/Varbase Subprofile/${subprofil
 grep -rl 'Varbase Sub Profile Basic Project' * | xargs sed -i "s/Varbase Sub Profile Basic Project/${subprofile_name} project/g"
 grep -rl 'varbase-subprofile-basic-project' * | xargs sed -i "s/varbase-subprofile-basic-project/${subprofile_name}-project/g"
 
+
+## Processor: Public Github finished generating.
+echo "=========================================================================";
+echo "  Processor: Public Github finished generating                           ";
+echo "=========================================================================";
